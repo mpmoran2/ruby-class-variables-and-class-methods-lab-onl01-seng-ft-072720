7a2f310598164395d7c5277a136511aa05633d31
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-06 15:16:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,6 +7,7 @@ class Song
    def initialize(name, artist, genre)
      @name = name
      @artist = artist
+     @genre = genre
      @@genres << @genre
      @@artists << @artist
      @@song_count +=1 
